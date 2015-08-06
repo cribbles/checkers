ffd23b556834591249d69ba90125e8c16589c702
Chris Sloop <csloop@wesleyan.edu>
2015-08-06 11:17:14 -0400

    Initialize Game class

---
+++ game.rb
@@ -0,0 +1,17 @@
+class Game
+  attr_reader :current_player
+
+  def initialize(players = nil)
+    players ||= {red: HumanPlayer.new(:red), blue: HumanPlayer.new(:blue)}
+
+    @red, @blue = players.first, players.last
+    @current_player = blue
+  end
+
+  def switch_players!
+    self.current_player = (current_player == blue) ? red : blue
+  end
+
+  private
+  attr_reader :players, :red, :blue
+end
