3d140ac73e5484d04dee8852888932322a83e2bb
Chris Sloop <csloop@wesleyan.edu>
2015-08-06 10:11:35 -0400

    Add moves to Piece

--- piece.rb
+++ piece.rb
@@ -1,9 +1,17 @@
 class Piece
+  MOVES = {
+    red:    [[1, -1], [1, 1]],
+    yellow: [[-1, 1], [-1, -1]]
+  }
+
+  attr_reader :moves
+
   def initialize(color)
     raise unless [:red, :yellow].include?(color)
 
     @color = color
     @king = false
+    @moves = MOVES[color]
   end
 
   def red?
